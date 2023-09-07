9cdfe5fbb753fe5ab01ff330a34533de49514e6a
StylexTV <stylextv42@gmail.com>
2023-09-07 13:58:28 +0200

    Refactored and optimized source code

--- src/io/bio/Bio.java
+++ src/io/bio/Bio.java
@@ -2,83 +2,86 @@ package io.bio;
 
 public class Bio {
 	
-	private static final char SMALLEST_HASH_STRING_CHARACTER_ID = 32;
-	private static final char LARGEST_HASH_STRING_CHARACTER_ID = 126;
+	private static final char SMALLEST_SEED_STRING_CHARACTER_ID = 32;
+	private static final char LARGEST_SEED_STRING_CHARACTER_ID = 126;
 	
-	private static final char DEFAULT_HASH_STRING_CHARACTER = 'O';
-	private static final char EMPTY_STRING_HASH = 0;
-	private static final char STRING_HASH_CHARACTER_WEIGHT_FACTOR = 31;
-	private static final char LAST_STRING_HASH_CHARACTER_WEIGHT = 1;
-	private static final char MINIMAL_REVERSE_HASH_STRING_LENGTH = 1;
+	private static final char EMPTY_STRING_SEED = 0;
+	private static final char MINIMAL_SEED_STRING_LENGTH = 1;
+	private static final char DEFAULT_SEED_STRING_CHARACTER = 'O';
+	private static final char SEED_STRING_CHARACTER_WEIGHT_FACTOR = 31;
+	private static final char LAST_SEED_STRING_CHARACTER_WEIGHT = 1;
 	
-	public static String reverseHashString(int hash, String hashStringPrefix) {
-		int h = hashString(hashStringPrefix);
-		int length = MINIMAL_REVERSE_HASH_STRING_LENGTH;
+	public static String simplifySeedString(String seedString) {
+		int seed = seed(seedString);
+		return seedString(seed);
+	}
+	
+	public static String seedString(int seed, String seedStringPrefix) {
+		int s = seed(seedStringPrefix);
+		int length = MINIMAL_SEED_STRING_LENGTH;
 		
 		while(true) {
 			
-			int h2 = h;
-			for(int i = 0; i < length; i++) h2 *= STRING_HASH_CHARACTER_WEIGHT_FACTOR;
-			h2 = hash - h2;
+			s *= SEED_STRING_CHARACTER_WEIGHT_FACTOR;
 			
-			String string = reverseHashString(h2, length);
-			if(string != null) return hashStringPrefix + string;
+			String seedString = seedString(seed - s, length);
+			if(seedString != null) return seedStringPrefix + seedString;
 			
 			length++;
 		}
 	}
 	
-	public static String reverseHashString(int hash) {
-		int length = MINIMAL_REVERSE_HASH_STRING_LENGTH;
+	public static String seedString(int seed) {
+		int length = MINIMAL_SEED_STRING_LENGTH;
 		
 		while(true) {
 			
-			String string = reverseHashString(hash, length);
-			if(string != null) return string;
+			String seedString = seedString(seed, length);
+			if(seedString != null) return seedString;
 			
 			length++;
 		}
 	}
 	
-	private static String reverseHashString(int hash, int length) {
+	private static String seedString(int seed, int length) {
 		char[] characters = new char[length];
 		int[] characterWeights = new int[length];
 		
-		int h = EMPTY_STRING_HASH;
-		int weight = LAST_STRING_HASH_CHARACTER_WEIGHT;
+		int s = EMPTY_STRING_SEED;
+		int weight = LAST_SEED_STRING_CHARACTER_WEIGHT;
 		
 		for(int characterIndex = length - 1; characterIndex >= 0; characterIndex--) {
 			
-			characters[characterIndex] = DEFAULT_HASH_STRING_CHARACTER;
-			h += DEFAULT_HASH_STRING_CHARACTER * weight;
+			characters[characterIndex] = DEFAULT_SEED_STRING_CHARACTER;
+			s += DEFAULT_SEED_STRING_CHARACTER * weight;
 			
 			characterWeights[characterIndex] = weight;
-			weight *= STRING_HASH_CHARACTER_WEIGHT_FACTOR;
+			weight *= SEED_STRING_CHARACTER_WEIGHT_FACTOR;
 		}
 		
-		boolean hashMatches = h == hash;
-		if(!hashMatches) {
+		boolean seedMatches = s == seed;
+		if(!seedMatches) {
 			
 			for(int characterIndex = 0; characterIndex < length; characterIndex++) {
 				
 				char c = characters[characterIndex];
 				weight = characterWeights[characterIndex];
 				
-				char character = enhanceHashStringCharacter(c, weight, h, hash);
-				int enhancedHash = h - c * weight + character * weight;
+				char character = enhanceSeedStringCharacter(c, weight, s, seed);
+				int enhancedSeed = s - c * weight + character * weight;
 				
-				h = enhancedHash;
+				s = enhancedSeed;
 				characters[characterIndex] = character;
 				
-				if(h == hash) {
-					
-					hashMatches = true;
+				if(s == seed) {
+
+					seedMatches = true;
 					break;
 				}
 			}
 		}
 		
-		if(!hashMatches) return null; 
+		if(!seedMatches) return null; 
 		
 		StringBuilder builder = new StringBuilder();
 		for(char character : characters) builder.append(character);
@@ -86,47 +89,54 @@ public class Bio {
 		return builder.toString();
 	}
 	
-	private static char enhanceHashStringCharacter(char character, int characterWeight, int hash, int targetHash) {
-		int characterID = (targetHash - (hash - character * characterWeight)) / characterWeight;
-		if(characterID >= SMALLEST_HASH_STRING_CHARACTER_ID && characterID <= LARGEST_HASH_STRING_CHARACTER_ID) {
+	private static char enhanceSeedStringCharacter(char character, int characterWeight, int seed, int targetSeed) {
+		int characterID = (targetSeed - (seed - character * characterWeight)) / characterWeight;
+		if(characterID >= SMALLEST_SEED_STRING_CHARACTER_ID && characterID <= LARGEST_SEED_STRING_CHARACTER_ID) {
 			
-			int h = hash - character * characterWeight + characterID * characterWeight;
-			if(h == targetHash) return (char) characterID;
+			int s = seed - character * characterWeight + characterID * characterWeight;
+			if(s == targetSeed) return (char) characterID;
 		}
 		
-		hash -= character * characterWeight;
+		seed -= character * characterWeight;
 		int bestCharacterID = character;
-		int bestHashDistance = Integer.MAX_VALUE;
+		long bestSeedDistance = Integer.MAX_VALUE;
 		
-		for(int id = SMALLEST_HASH_STRING_CHARACTER_ID; id <= LARGEST_HASH_STRING_CHARACTER_ID; id++) {
+		for(int id = SMALLEST_SEED_STRING_CHARACTER_ID; id <= LARGEST_SEED_STRING_CHARACTER_ID; id++) {
 			
-			hash += id * characterWeight;
-			int hashDistance = Math.abs(hash - targetHash);
-			hash -= id * characterWeight;
+			seed += id * characterWeight;
+			long seedDistance = seedDistance(seed, targetSeed);
+			seed -= id * characterWeight;
 			
-			if(hashDistance < bestHashDistance) {
+			if(seedDistance < bestSeedDistance) {
 				
 				bestCharacterID = id;
-				bestHashDistance = hashDistance;
+				bestSeedDistance = seedDistance;
 			}
 		}
 		
 		return (char) bestCharacterID;
 	}
 	
-	public static int hashString(String string) {
-		int hash = 0;
-		int weight = 1;
-		char[] characters = string.toCharArray();
+	private static long seedDistance(int seed1, int seed2) {
+		long distance1 = Math.abs(seed1 - seed2);
+		long distance2 = Math.abs((long) Math.min(seed1, seed2) - Integer.MIN_VALUE) + Math.abs((long) Math.max(seed1, seed2) - Integer.MAX_VALUE) + 1;
+		
+		return Math.min(distance1, distance2);
+	}
+	
+	public static int seed(String seedString) {
+		int seed = 0;
+		int weight = LAST_SEED_STRING_CHARACTER_WEIGHT;
+		char[] characters = seedString.toCharArray();
 		
 		for(int i = characters.length - 1; i >= 0; i--) {
 			char character = characters[i];
-			
-			hash += character * weight;
-			weight *= 31;
+
+			seed += character * weight;
+			weight *= SEED_STRING_CHARACTER_WEIGHT_FACTOR;
 		}
 		
-		return hash;
+		return seed;
 	}
 	
 }
--- src/io/bio/BioTest.java
+++ src/io/bio/BioTest.java
@@ -3,11 +3,10 @@ package io.bio;
 public class BioTest {
 	
 	public static void main(String[] args) {
-		int hash = 69420;
+		String seedString = "TotallyAwesomeButPrettyLongSeed";
 		
-		String s = Bio.reverseHashString(hash);
-		System.out.println(s);                 // prints 'FCI'
-		System.out.println(Bio.hashString(s)); // prints '69420', as expected
+		seedString = Bio.simplifySeedString(seedString);
+		System.out.println(seedString); // prints 'MKCMEO'
 	}
 	
 }
